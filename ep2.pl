65526bb9985fc7e7b2dc9736af04bee63f894358
Mark Heath <mark@silicontrip.net>
2013-01-11 23:52:53 +1100

    Added series and episode number padding.

--- ep2.pl
+++ ep2.pl
@@ -12,11 +12,16 @@ while ($filename = shift) {
 	
 	$episode = $epfac->episode($filename);
 	
-	print "$filename: series: " . $episode->seriesNumber() . " episode: " . $episode->episodeNumber() . " Series Name: " . $episode->seriesName() . "\n";
+	print "$filename\n"; 
+	#print "series: " . $episode->seriesNumber() . "\n";
+	#print "episode: " . $episode->episodeNumber() . "\n";
+	#print "Series Name: " . $episode->seriesName() . "\n";
 	
 	$meta = new AVMeta($filename);
 	
-	print $meta->getVideoString() . "\n";	
-	print $meta->getAudioString() . "\n";
+	#	print $meta->getVideoString() . "\n";	
+	#print $meta->getAudioString() . "\n";
+	
+	print $episode->seriesName() . "-" . "s" . $episode->seriesNumber() . "e" . $episode->episodeNumber() . "." . $meta->getVideoString() . "." . $meta->getAudioString() . "\n";
 	
 }
